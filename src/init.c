881d5b307f3eec6deab1da221ae1b580299fd132
Luna del Valle <luna7111@tuta.io>
2025-03-06 11:42:00 +0100

    Added gctrl_init function

---
+++ src/init.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   init.c                                         .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2025/03/05 18:03:02 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2025/03/05 18:11:24 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <garbage_control.h>
+
+t_gctrl	*gctrl_init(void)
+{
+	t_gctrl	*dump;
+
+	dump = NULL;
+	return (dump);
+}
+//initializes the list (just returns a t_gctrl equalled to NULL)
