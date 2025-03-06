9f94da4e81e6253c03fd16bc9dd32372639de4a8
Luna del Valle <luna7111@tuta.io>
2025-03-06 19:42:34 +0100

    Added structure for the user (contains the pointer to the linked list, but doesnt need to be refered as a double pointer)

--- src/garbage_control.h
+++ src/garbage_control.h
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/03/05 17:52:56 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/03/05 18:11:45 by ldel-val          ``                     */
+/*   Updated: 2025/03/06 18:56:50 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,28 @@
 typedef char t_memtype;
 
 # define GC_FREE 0
-# define GC_PRT 1
+# define GC_PTR 1
 # define GC_STRARRAY 2
 
-typedef struct s_gctrl
+typedef struct s_gcrtl_dump
 {
 	t_memtype		type;
 	void			*ptr;
 	struct s_node	*next
 
-}	t_gctrl;
+}	t_gctrl_dump;
+
+typedef struct s_gcrtl
+{
+	t_gctrl_list	**dump;
+}
+
+void	dump_add_ptr(t_gctrl_dump **dump, void *ptr, t_memtype type);
 
 t_gctrl	*gctrl_init(void);
+
+void	*gctrl_malloc(t_gctrl *dump, size_t size);
+void	gctrl_free(t_gctrl *dump, void *ptr);
+void	*gctrl_calloc(t_gctrl *dump, size_t nmemb, size_t size);
 
 #endif
