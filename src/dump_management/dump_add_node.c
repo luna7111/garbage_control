1fd5d8d60588c623db01cc95b772e38eae3b07d9
Luna del Valle <luna7111@tuta.io>
2025-03-21 23:23:51 +0100

    Added function dump_add_ptr

--- src/dump_management/dump_add_node.c
+++ src/dump_management/dump_add_node.c
@@ -6,21 +6,22 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/03/06 12:26:24 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/03/06 18:06:15 by ldel-val          ``                     */
+/*   Updated: 2025/03/17 16:40:09 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <garbage_control.h>
 
-void	dump_add_ptr(t_gctrl_dump **dump, void *ptr, t_memtype type)
+void	dump_add_ptr(t_gctrl_dump **dump, void *ptr, int block, t_memtype type)
 {
 	t_gctrl_dump	*new_node;
 
 	new_node = malloc(sizeof(t_gctrl_dump));
 	if (!new_node)
 		return ;
-	new_node->ptr = ptr;
-	new_node->type = type;
 	new_node->next = *dump;
 	*dump = new_node;
+	(*dump)->ptr = ptr;
+	(*dump)->block = block;
+	(*dump)->type = type;
 }
