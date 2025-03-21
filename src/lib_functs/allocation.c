0f1d6485559ae298c998beb136a4b13866f3ec61
Luna del Valle <luna7111@tuta.io>
2025-03-21 23:13:55 +0100

    Added block to functions, added gctrl free

--- src/lib_functs/allocation.c
+++ src/lib_functs/allocation.c
@@ -6,29 +6,30 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/03/05 18:14:25 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/03/06 20:13:53 by ldel-val          ``                     */
+/*   Updated: 2025/03/20 19:10:49 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <garbage_control.h>
 
-void	*gctrl_malloc(t_gctrl *gctrl, size_t size)
+void	*gctrl_malloc(t_gctrl *gctrl, int block, size_t size)
 {
 	void	*ptr;
 
 	ptr = malloc(size);
 	if (!ptr)
 		return (NULL);
-	dump_add_ptr(gctrl->dump, ptr, GC_PTR);
+	dump_add_ptr(gctrl->dump, ptr, block, GC_PTR);
 	return (ptr);
 }
-/*
+
 void	gctrl_free(t_gctrl *gctrl, void *ptr)
 {
-
+	dump_remove_ptr(gctrl->dump, get_node(*(gctrl->dump), ptr));
+	free(ptr);
 }
-*/
-void	*gctrl_calloc(t_gctrl *gctrl, size_t nmemb, size_t size)
+
+void	*gctrl_calloc(t_gctrl *gctrl, int block, size_t nmemb, size_t size)
 {
 	void	*ptr;
 	size_t	i;
@@ -42,6 +43,6 @@ void	*gctrl_calloc(t_gctrl *gctrl, size_t nmemb, size_t size)
 		((char *)ptr)[i] = 0;
 		i ++;
 	}
-	dump_add_ptr(gctrl->dump, ptr, GC_PTR);
+	dump_add_ptr(gctrl->dump, ptr, block, GC_PTR);
 	return (ptr);
 }
