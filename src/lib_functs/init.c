85c74b0343212da3d6eca53464cd21e6857f8571
Luna del Valle <luna7111@tuta.io>
2025-03-21 23:12:43 +0100

    Fixed an error where the first's node was allocated on stack

--- src/lib_functs/init.c
+++ src/lib_functs/init.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/03/05 18:03:02 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/03/05 18:11:24 by ldel-val          ``                     */
+/*   Updated: 2025/03/14 17:07:57 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 t_gctrl	*gctrl_init(void)
 {
-	t_gctrl	*dump;
+	t_gctrl_dump	**dump;
+	t_gctrl			*gctrl;
 
-	dump = NULL;
-	return (dump);
+	gctrl = malloc(sizeof(t_gctrl));
+	dump = malloc(sizeof(t_gctrl_dump *));
+	*dump = NULL;
+	gctrl->dump = dump;
+	return (gctrl);
 }
 //initializes the list (just returns a t_gctrl equalled to NULL)
